7ca535bb7a950699315fcbf6f81fceca14c4054f
thag15 <2000003730@nttu.edu.vn>
2023-03-09 21:15:10 +0700

    up data json

--- src/components/Body/carousel/Home_Carousel.js
+++ src/components/Body/carousel/Home_Carousel.js
@@ -1,5 +1,6 @@
 import "./Home_Carousel.scss";
 import React, { useState } from "react";
+import Stores from "../../../data/stores.json";
 export default function HomeCarousel() {
   const [active, setActive] = useState(false);
   const handleOnclick = () => {
@@ -15,96 +16,26 @@ export default function HomeCarousel() {
         </div>
 
         <ul className="list_product">
-          <li>
-            <div className="list_product_img">
-              <img
-                src="https://scontent.fsgn1-1.fna.fbcdn.net/v/t1.15752-9/329193793_3414655672083245_3006518930087245934_n.png?_nc_cat=108&ccb=1-7&_nc_sid=ae9488&_nc_ohc=T0vvnKPKIvAAX9IVwgL&_nc_ht=scontent.fsgn1-1.fna&oh=03_AdTcXeKIMPRuYRD6eE1uIkIO8WAuJ2-ZgVd52KYIpBnn_Q&oe=64266D56"
-                alt=""
-              />
-            </div>
-            <div className="infor_list_product">
-              <p className="name_product">Name-product</p>
-              <p className="price_product">$200</p>
-              <button type="button" className="add_product  btn btn-danger">
-                Thêm vào giỏ
-              </button>
-            </div>
-          </li>
-          <li>
-            <div className="list_product_img">
-              <img
-                src="https://ik.imagekit.io/fruitcompany/H%C3%ACnh_c%C3%B3_gi%C3%A1/2?ik-sdk-version=javascript-1.4.3&updatedAt=1677337524550"
-                alt=""
-              />
-            </div>
-            <div className="infor_list_product">
-              <p className="name_product">Name-product</p>
-              <p className="price_product">$200</p>
-              <button type="button" className="add_product  btn btn-danger">
-                Thêm vào giỏ
-              </button>
-            </div>
-          </li>
-          <li>
-            <div className="list_product_img">
-              <img
-                src="https://ik.imagekit.io/fruitcompany/H%C3%ACnh_c%C3%B3_gi%C3%A1/2?ik-sdk-version=javascript-1.4.3&updatedAt=1677337524550"
-                alt=""
-              />
-            </div>
-            <div className="infor_list_product">
-              <p className="name_product">Name-product</p>
-              <p className="price_product">$200</p>
-              <button type="button" className="add_product  btn btn-danger">
-                Thêm vào giỏ
-              </button>
-            </div>
-          </li>
-          <li>
-            <div className="list_product_img">
-              <img
-                src="https://ik.imagekit.io/fruitcompany/H%C3%ACnh_c%C3%B3_gi%C3%A1/2?ik-sdk-version=javascript-1.4.3&updatedAt=1677337524550"
-                alt=""
-              />
-            </div>
-            <div className="infor_list_product">
-              <p className="name_product">Name-product</p>
-              <p className="price_product">$200</p>
-              <button type="button" className="add_product  btn btn-danger">
-                Thêm vào giỏ
-              </button>
-            </div>
-          </li>
-          <li>
-            <div className="list_product_img">
-              <img
-                src="https://ik.imagekit.io/fruitcompany/H%C3%ACnh_c%C3%B3_gi%C3%A1/2?ik-sdk-version=javascript-1.4.3&updatedAt=1677337524550"
-                alt=""
-              />
-            </div>
-            <div className="infor_list_product">
-              <p className="name_product">Name-product</p>
-              <p className="price_product">$200</p>
-              <button type="button" className="add_product  btn btn-danger">
-                Thêm vào giỏ
-              </button>
-            </div>
-          </li>
-          <li>
-            <div className="list_product_img">
-              <img
-                src="https://ik.imagekit.io/fruitcompany/H%C3%ACnh_c%C3%B3_gi%C3%A1/2?ik-sdk-version=javascript-1.4.3&updatedAt=1677337524550"
-                alt=""
-              />
-            </div>
-            <div className="infor_list_product">
-              <p className="name_product">Name-product</p>
-              <p className="price_product">$200</p>
-              <button type="button" className="add_product  btn btn-danger">
-                Thêm vào giỏ
-              </button>
-            </div>
-          </li>
+          {Stores &&
+            Stores.map((item) => {
+              return (
+                <li key={item.id}>
+                  <div className="list_product_img">
+                    <img src={item.imgUrl} alt="" />
+                  </div>
+                  <div className="infor_list_product">
+                    <p className="name_product">{item.name}</p>
+                    <p className="price_product">{item.price}</p>
+                    <button
+                      type="button"
+                      className="add_product  btn btn-danger"
+                    >
+                      Thêm vào giỏ
+                    </button>
+                  </div>
+                </li>
+              );
+            })}
         </ul>
       </div>
     </>
