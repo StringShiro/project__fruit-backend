59cd365c3e5b27ddd49fde8799f60d493c35ac49
Andrew <trieuvyxka1@gmail.com>
2023-03-12 12:11:28 +0700

    update responsive profile page 12/3

--- src/components/Body/carousel/Home_Carousel.js
+++ src/components/Body/carousel/Home_Carousel.js
@@ -6,6 +6,19 @@ export default function HomeCarousel() {
   const handleOnclick = () => {
     console.log(active);
   };
+
+  function getData() {
+    const product = "http://localhost:3002/api/products";
+    fetch(product)
+      .then((res) => {
+        return res.json();
+      })
+      .then(function(){})
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
     <>
       <div className="Home_Carousel">
@@ -14,28 +27,28 @@ export default function HomeCarousel() {
           <h5 onClick={handleOnclick}>Sản phẩm khuyến mãi</h5>
           <h5 onClick={handleOnclick}>Sản phẩm mới</h5>
         </div>
-
         <ul className="list_product">
-          {Stores &&
-            Stores.map((item) => {
-              return (
-                <li key={item.id}>
-                  <div className="list_product_img">
-                    <img src={item.imgUrl} alt="" />
-                  </div>
-                  <div className="infor_list_product">
-                    <p className="name_product">{item.name}</p>
-                    <p className="price_product">{item.price}</p>
-                    <button
-                      type="button"
-                      className="add_product  btn btn-danger"
-                    >
-                      Thêm vào giỏ
-                    </button>
-                  </div>
-                </li>
-              );
-            })}
+          {getData((data) => {
+            data.map((item) => {
+              return `
+                  <li >
+                    <div className="list_product_img">
+                      <img src=${item.image} alt="" />
+                    </div>
+                    <div className="infor_list_product">
+                      <p className="name_product">${item.productname}</p>
+                      <p className="price_product">${item.cost}</p>
+                      <button
+                        type="button"
+                        className="add_product  btn btn-danger"
+                      >
+                        Thêm vào giỏ
+                      </button>
+                    </div>
+                  </li>
+                `;
+            });
+          })}
         </ul>
       </div>
     </>
